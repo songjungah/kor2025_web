2269108e714ee7aa1308cdc22710628fc96f2aa1
songjungah <1104wjddk@naver.com>
2025-01-20 21:39:43 +0900

    250120

--- src/main/java/korweb/controller/ViewController.java
+++ src/main/java/korweb/controller/ViewController.java
@@ -7,9 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
+    // [1] 메인 페이지를 반환해주는 메소드
     @GetMapping("")     // http://localhost:8080
     public String index() {
         return "index.html";
+    }
+
+    // [2] 로그인 페이지를 반환해주는 메소드
+    @GetMapping("/member/login")
+    public String login() {
+        return "/member/login.html";
+    }
+
+    // [3] 회원가입 페이지를 반환해주는 메소드
+    @GetMapping("/member/signup")
+    public String signup() {
+        return "member/signup.html";
     }
 
 }
