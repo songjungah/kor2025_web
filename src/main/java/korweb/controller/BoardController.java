2d4fd80a3cee093691f12c6b78aa83a055a75a35
songjungah <1104wjddk@naver.com>
2025-02-12 21:13:30 +0900

    250212

--- src/main/java/korweb/controller/BoardController.java
+++ src/main/java/korweb/controller/BoardController.java
@@ -1,11 +1,14 @@
 package korweb.controller;
 
 import korweb.model.dto.BoardDto;
+import korweb.model.dto.PageDto;
 import korweb.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class BoardController {
@@ -23,10 +26,13 @@ public class BoardController {
 //        return boardService.boardFindAll();
 //    }
 
-    // [2] 게시물 전체 조회
+    // [2] 카테고리별 게시물 전체 조회 + 페이징처리( vs 무한스크롤)
+    // http://localhost:8080/board/findall.do?cno=1&page=1 : 1번(뉴스) 카테고리의 1번 페이지 조회
+    // http://localhost:8080/board/findall.do?cno=1&page=3 : 1번(뉴스) 카테고리의 3번 페이지 조회
+    // http://localhost:8080/board/findall.do?cno=3&page=2 : 3번(FAQ) 카테고리의 2번 페이지 조회
     @GetMapping("/board/findall.do")
-    public List<BoardDto> boardFindAll(@RequestParam int cno) { // 조회할 카테고리 번호
-        return boardService.boardFindAll(cno);
+    public PageDto boardFindAll(@RequestParam int cno, @RequestParam int page) { // 조회할 카테고리 번호, 페이지 번호
+        return boardService.boardFindAll(cno, page);
     }
 
     // [3] 게시물 특정(개별) 조회
@@ -43,6 +49,19 @@ public class BoardController {
     @DeleteMapping("/board/delete.do")
     public boolean boardDelete( @RequestParam int bno ){
         return boardService.boardDelete( bno );
+    }
+
+    // ========== 댓글 ========== //
+    // [6] 댓글 쓰기
+    @PostMapping("/reply/write.do")
+    public boolean replyWrite(@RequestBody Map<String, String> replyDto) {   // Dto 클래스 대신에 map 컬렉션 활용
+        return boardService.replyWrite(replyDto);
+    }
+
+    // [7] 특정 게시물의 댓글 전체 조회
+    @GetMapping("/reply/findall.do")
+    public List<Map<String, String>> replyFindAll(@RequestParam int bno) {
+        return boardService.replyFindAll(bno);
     }
 
 }
--- src/main/resources/static/js/board/write.js
+++ src/main/resources/static/js/board/write.js
@@ -1,8 +1,9 @@
+
 // 썸머노트 실행
 $(document).ready(function() {
     $('#summernote').summernote({
         height : 500,    // 썸머노트 게시판의 높이조절 속성
-        lang : 'ko-KR',  // 썸머노트 메뉴 한글화 
+        lang : 'ko-KR',  // 썸머노트 메뉴 한글화
         placeholder : '게시물 내용을 입력해주세요.' // 입력 전에 가이드라인 제공 속성
     });
   });
